ee8e88a56edd52390bbae60d4941c301fa6c27f4
Swizec Teller <swizec@swizec.com>
2014-05-03 16:02:49 -0700

    getting links html part

--- downloader.py
+++ downloader.py
@@ -1,10 +1,32 @@
 
 import urlparse, os, requests
+from pyquery import PyQuery as pq
+
+def process(dossier):
+    #url = url(dossier)
+    path = os.path.normpath(os.path.join(os.getcwd(),
+                                         "data", dossier))
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    print "Fetching %s" % dossier
+    html = fetch(url(dossier))
+
+    print "Storing %s" % dossier
+    if not os.path.exists(os.path.join(path, "main.html")):
+        store(os.path.join(path, "main.html"), html)
+
+    print "Finding sublinks"
+    print sublinks(os.path.join(path, "main.html"))
+
+def store(path, text):
+    f = open(path, "w")
+    f.write(text)
+    f.close()
 
 def fetch(url):
     r = requests.get(url)
-    return r.text
-
+    return r.text.encode('utf-8')
 
 def url(dossier):
     dossier = dossier.strip()
@@ -15,7 +37,11 @@ def url(dossier):
                                              "%s_%s.html" % (dossier, dossier)),
                                 "", "", ""))
 
+def sublinks(file):
+    d = pq(filename=file)
+    print d("#toc")
+
 if __name__ == '__main__':
     for line in open("dossiers.txt"):
-        text = fetch(url(line))
-        print len(text)
+        process(line.strip())
+        break
