3f32be300ec04e5673895be8aabe2655a7946dd0
Swizec Teller <swizec@swizec.com>
2014-05-04 16:21:34 -0700

    let's keep a list of dependencies shall we

--- downloader.py
+++ downloader.py
@@ -3,21 +3,23 @@ import urlparse, os, requests
 from pyquery import PyQuery as pq
 
 def process(dossier):
+    print "Processing %s" % dossier
+    print url(dossier)
     #url = url(dossier)
     path = os.path.normpath(os.path.join(os.getcwd(),
                                          "data", dossier))
     if not os.path.exists(path):
         os.mkdir(path)
 
-    print "Fetching %s" % dossier
-    html = fetch(url(dossier))
-
-    print "Storing %s" % dossier
     if not os.path.exists(os.path.join(path, "main.html")):
+        print "Fetching"
+        html = fetch(url(dossier))
+
+        print "Storing"
         store(os.path.join(path, "main.html"), html)
 
     print "Finding sublinks"
-    print sublinks(os.path.join(path, "main.html"))
+    print sublinks(os.path.join(path, "main.html"), url)
 
 def store(path, text):
     f = open(path, "w")
@@ -37,9 +39,23 @@ def url(dossier):
                                              "%s_%s.html" % (dossier, dossier)),
                                 "", "", ""))
 
-def sublinks(file):
+def sublinks(file, base_url):
     d = pq(filename=file)
-    print d("#toc")
+    name(pq(d("#toc")("a")[2]))
+    #print [(name(pq(a)), pq(a).attr("href")) for a in d("#toc")("a")]
+
+def name(link):
+    name = link.text()
+
+    parent = link.parent()
+    while parent[0].tag != "div":
+        if parent[0].tag == "ul" and parent.siblings("p").length > 0:
+            name = "%s-%s" % (parent.siblings("p").text(), name)
+
+        parent = parent.parent()
+
+    return name
+
 
 if __name__ == '__main__':
     for line in open("dossiers.txt"):
